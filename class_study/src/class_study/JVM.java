c5452f1e3aa09ae9faf755f2d3668b9ff3a84986
hyeminyy <www_wowwh@naver.com>
2024-09-05 23:29:43 +0900

    JVM 예시 코드 (수정)

--- class_study/src/class_study/JVM.java
+++ class_study/src/class_study/JVM.java
@@ -17,3 +17,9 @@ public class JVM {
 		System.out.println("main(String[] args)이 종료되었음.");
 	}
 }
+
+//JVM 메모리 구조
+//JVM은 시스템으로부터 프로그램을 수행하는데 필요한 메모리를 할당받고,
+//JVM은 이 메모리를 용도에 따라 여러 영역으로 나누어 관리
+
+//https://steady-developer-hyemin.tistory.com/84 
